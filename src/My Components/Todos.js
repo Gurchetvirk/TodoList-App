e7fac9a5928bc75e33d3e9a20012f6da5b0194d3
Gurchetvirk <gurchets2599@gmail.com>
2021-11-05 23:00:12 +0530

    Seond Revision

---
+++ src/My Components/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import todoItem from '../My Components/Todo-item'
+
+export default function Todos(props) {
+    return (
+        <div>
+            <todoItem />
+        </div>
+    )
+}
+
